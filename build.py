5d3f377314999a9dd06fa2f45431a1cbdcf308e3
Linus Kloeckner <linus.kloeckner@googlemail.com>
2019-01-26 10:42:46 +0100

    Split mingw builds into Debug and Release and enforce libstdc++11

--- build.py
+++ build.py
@@ -1,7 +1,18 @@
 from cpt.packager import ConanMultiPackager
+import platform
 
 
 if __name__ == "__main__":
     builder = ConanMultiPackager()
     builder.add_common_builds(shared_option_name=False, pure_c=False)
+
+    if platform.system() == "Windows":
+        filtered_builds = []
+        for settings, options, env_vars, build_requires in builder.builds:
+            # MinGW should use "libstdc++11"
+            if settings["compiler"] == "gcc":
+                settings["compiler.libcxx"] = "libstdc++11"
+            filtered_builds.append([settings, options, env_vars, build_requires])
+        builder.builds = filtered_builds
+
     builder.run()
