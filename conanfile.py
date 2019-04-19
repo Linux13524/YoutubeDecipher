5cac2a5f9c2aed838bdd0fd773966cb952b500b3
Linus Klöckner <linus.kloeckner@googlemail.com>
2019-04-19 17:07:45 +0200

    Fix conanfile for several conan hooks
    Add homepage
    Remove fPIC for msvc
    Export license and pack it

--- conanfile.py
+++ conanfile.py
@@ -8,13 +8,19 @@ class YoutubeDecipherConan(ConanFile):
     author = "Linus Kloeckner (linus.kloeckner@googlemail.com)"
     url = "https://github.com/Linux13524/YoutubeDecipher"
     description = "Library to decipher youtube signatures"
+    homepage = "https://www.linux13524.de"
     topics = ("youtube", "decipher", "signature")
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = "shared=False", "fPIC=True"
     requires = "boost/1.69.0@conan/stable", "cpr/1.3.0@linux13524/stable"
     generators = "cmake"
+    exports = ["LICENSE.md"]
     exports_sources = "package/*"
+
+    def configure(self):
+        if self.settings.compiler == 'Visual Studio':
+            del self.options.fPIC
 
     def build(self):
         cmake = CMake(self)
@@ -28,6 +34,8 @@ class YoutubeDecipherConan(ConanFile):
         # Copy sources for debugging
         if self.settings.build_type == "Debug":
             self.copy("*.cpp", dst="src", src="package/src")
+        # Copy the license files
+        self.copy("LICENSE.md", dst="licenses")
 
     def package_info(self):
         LIB_POSTFIX = "-d" if self.settings.build_type == "Debug" else ""
