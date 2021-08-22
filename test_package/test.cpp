478685794c201f2006f1728b90f6df2a8c8b3874
Linus Klöckner <mail@linus-kloeckner.de>
2021-08-22 15:04:53 +0200

    Small fix in test_package

--- test_package/test.cpp
+++ test_package/test.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Trying to download https://www.youtube.com/watch?v=s8XIgR5OGJc ... ";
+    std::cout << "Trying to download https://www.youtube.com/watch?v=s8XIgR5OGJc ... " << std::endl;
     auto html_response = cpr::Get(cpr::Url{"https://www.youtube.com/watch?v=s8XIgR5OGJc"}, cpr::VerifySsl{false});
     std::cout << "Trying to load decipher ... ";
     Youtube::Decipher::Instance(html_response.text);
